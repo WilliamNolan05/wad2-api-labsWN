2fb3897278ce219f389f455ab66e7c77c5abc723
William <20107153@mail.wit.ie>
2025-12-11 17:59:31 +0000

    Routes for get task and task details

---
+++ tasky-api/api/tasks/tasksData.js
@@ -0,0 +1,8 @@
+export const tasksData = {
+    page: 1, 
+    total_pages: 1, 
+    total_results: 3,
+    tasks: [{id: "1", title: "Dishes", description: "Empty Dishwasher", deadline: "today", done: false, priority: "low"},
+            {id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium"},
+            {id: "3", title: "Grocery Shopping", description: "Tidy up", deadline: "Today", done: false, priority: "high"}]
+};
